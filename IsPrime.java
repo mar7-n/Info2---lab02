a3258845e8c6bf71300996121dfeeb21928d452d
mar7-n <116806411+mar7-n@users.noreply.github.com>
2022-11-22 16:34:09 +0100

    Update IsPrime.java

--- IsPrime.java
+++ IsPrime.java
@@ -1,25 +1,36 @@
-package LAB02;
+package lab02;
 
-public class IsPrime {
+public class lab02 {
+    static long count;
 
-    public static void main(String[] args) {
-        int i, n = 5;
-        boolean isprime = true;
+    public static void main(String[] args){
+        //h.fragment1(4);
+        long num = 549755813888L; // lowest number with 40 bits
+        for(int i=0;i<100;i++){
+            String result = isPrime(num) ? "Prime" : "not Prime";
+            System.out.println("The number " + num + " is: " + result + " and the for ran " +
+            count + " times.\n");
+            num++;
+        }
+    }
+
+    public static boolean isPrime(long n){
+        boolean isPrime = true;
+        count = 0L;
 
         // 0 and 1 are not prime numbers also, negative numbers are not prime
-        if (n < 2) {
-            isprime = false;
+        if (n < 2L) {
+            isPrime = false;
         } else {
-            for (i = 2; i < n; i++) {
-                if (n % i == 0) {
-                    isprime = false;
+            for (long i = 2L; i <= n/2L; i++) {
+                if (n % i == 0L) {
+                    isPrime = false;
                     break;
                 }
+                count++;
             }
         }
-
-        String result = isprime ? "Prime" : "not Prime";
-        System.out.println("The number " + n + " is : " + result);
+        return isPrime;
     }
 }
 // Time Complexity : O(N)
